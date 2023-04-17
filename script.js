5ff898b47b23beeaddbbb5075b814334e4c19d94
eah645 <92736056+eah645@users.noreply.github.com>
2023-04-17 14:52:36 -0400

    Fixed carousel

--- script.js
+++
@@ -1,9 +0,0 @@
-//Nav function
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
