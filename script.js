3632711cb41de0f7db5bfe1aebefbfc92f376090
eah645 <92736056+eah645@users.noreply.github.com>
2023-04-20 14:56:33 -0400

    More updates to education
    Animations are still needed that- might take me a while

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Nav function
+//Nav function (Idk why this- idk what this does)
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,4 +6,19 @@ function myFunction() {
     } else {
       x.className = "topnav";
     }
+  }
+
+  //Alright that work experience thing... I need to make a thing where when you reach a specific point it adds / removes a class I guess?
+  //Yeah the html and current class not the span ones
+  function educationAnimation() {
+    $(".school-lists").inViewport(
+      function () {
+        $(this).addClass("html");
+        $(this).addClass("current");
+      },
+      function () {
+        $(this).removeClass("html");
+        $(this).removeClass("current");
+      }
+    );
   }
