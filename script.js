2fef9e2cd7c9e29651bec5d5371cf6a0cbafce79
eah645 <92736056+eah645@users.noreply.github.com>
2023-04-30 19:50:33 -0400

    Added horizontal scroll on workExp nav

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 //Nav function (Idk why this- idk what this does)
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//Alright that work experience thing... I need to make a thing where when you reach a specific point it adds / removes a class I guess?
+//Yeah the html and current class not the span ones
+function educationAnimation() {
+  $(".school-lists").inViewport(
+    function () {
+      $(this).addClass("html");
+      $(this).addClass("current");
+    },
+    function () {
+      $(this).removeClass("html");
+      $(this).removeClass("current");
     }
-  }
+  );
+}
 
-  //Alright that work experience thing... I need to make a thing where when you reach a specific point it adds / removes a class I guess?
-  //Yeah the html and current class not the span ones
-  function educationAnimation() {
-    $(".school-lists").inViewport(
-      function () {
-        $(this).addClass("html");
-        $(this).addClass("current");
-      },
-      function () {
-        $(this).removeClass("html");
-        $(this).removeClass("current");
-      }
-    );
-  }
-
-  //Workexp function that might actually work-
-  //Write the function so it adds a class that keeps the altered color while clicked onto and removes it once clicked off
-  function workButton() {
-    document.getElementByClassName("workBtn").addClass(".workBtn-active");
-  }
+//Workexp function that might actually work-
+//Write the function so it adds a class that keeps the altered color while clicked onto and removes it once clicked off
+function workButton() {
+  document.getElementByClassName("workBtn").addClass(".workBtn-active");
+}
