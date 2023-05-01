a7e86d0c3c4efbb7f3fe34aef190c1a0b04fb7eb
eah645 <92736056+eah645@users.noreply.github.com>
2023-05-01 14:44:18 -0400

    Kinda made animation on nav work?
    It's still- so it works but it doesn't stand out which I need it to do like I can't customize it?

--- script.js
+++ script.js
@@ -28,3 +28,24 @@ function educationAnimation() {
 function workButton() {
   document.getElementByClassName("workBtn").addClass(".workBtn-active");
 }
+
+//WorkExp navbar
+//WAIT NO LOL THIS WORKS BUT IT'S REMOVING THE WRONG ACTIVE CLASS- OK DO A NEW CLASS THAT'LL FIX IT IDK THIS WAS FUNNY TO ME
+//Credit: https://www.youtube.com/watch?v=qARueNQFk04&t=479s
+const navLi = document.querySelectorAll('a')
+const sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach(section => {
+    let sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute('id');
+    }
+  });
+  navLi.forEach(a => {
+    //Ok no what the fuck WHAT CLASS ARE YOU REMOVING- YOU WORK ON WORKEXP ONLY AFTER USING THIS FUNCTION
+    a.classList.remove('active')
+    document.querySelector('a[href*= ' + current + ']').classList.add('active');
+  });
+});
